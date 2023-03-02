a7d13aa91da51465a82186274aeb90f77240551b
berkayie8 <ercanberkay103@gmail.com>
2023-03-03 01:29:44 +0300

    Add project files.

---
+++ YTDERS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTDERS
+{
+    //C# bilgisayar dili birçok türde uygulama yazabilmenizi sağlar, örneğin:
+
+    //Verileri yakalamak, analiz etmek ve işlemek için iş uygulamaları
+    //Web tarayıcısından erişilebilen dinamik web uygulamaları
+    //Oyunlar, hem 2B hem de 3B
+    //Finansal ve bilimsel uygulamalar
+    //Bulut tabanlı uygulamalar
+    //Mobil uygulamalar
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
