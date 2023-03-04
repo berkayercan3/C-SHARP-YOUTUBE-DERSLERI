c468ae6f6f05a396aa5e455cf4334a972431e0fe
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-03-04 04:59:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILkPROJE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
+
+            string name = "Bob";
+            int messages = 3;
+            decimal temparature = 34.4m;
+
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.Write("You have ");
+            Console.Write(messages);
+            Console.Write(" messages in your inbox. ");
+            Console.Write("The Temperature is ");
+            Console.Write(temparature);
+            Console.Write("cesius");
+            Console.ReadLine();
+
+        }
+    }
+}
