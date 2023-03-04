c24443736f72c9d655ff59457b17ed230fdff506
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-03-04 04:58:18 +0300

    DERS [0,2] KODLAR

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // SABIT VE DEGISKEN DEGERLER
+            //console.WriteLine('8');
+            //Console.WriteLine(12.50m);
+            //Console.WriteLine(true);
+            //Console.WriteLine("true");
+            //string , int , decimal , bool
+            //string firstname;
+            //firstname = "Berkay";
+            //int Deger;
+            //Deger = 0;
+            //Console.WriteLine(firstname);
+            //Console.WriteLine(Deger);
+            var mesage = "Hello World";
+            var sayi = 13;
+
+            Console.ReadLine();
+        }
+    }
+}
