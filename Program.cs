c79109ebbe6b7500995aaf2d3bb129a79eb2767b
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-03-21 14:22:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Bir satin alma sitesinde gun icerisinde belli basli
+            //siparislerde bir dolandirilma mevzusu olmus.
+            //Her siparisin kendine ait bir id si vardir ve 
+            //bu id lerin baslangic harfi B ile baslayanlarda dolandirilma olmus,
+            //bu idleri kullaniciya gsteren kodu yaziniz.
+            //B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179
+
+            string[] sıparıslerID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+            foreach(string IDLER in sıparıslerID)
+            {
+                if(IDLER.StartsWith("B"))
+                {
+                    Console.WriteLine(IDLER);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
