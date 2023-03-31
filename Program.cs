e064618d410f987acb42ddae94420b72a9c88630
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-03-31 03:03:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//string isim = "berkay";
+//string soyisim ="ercan";
+
+//if (isim == "berkay")
+//{
+
+//    Console.WriteLine($"{isim}");
+//    Console.WriteLine($"{soyisim}");
+//}
+//Console.WriteLine($"{soyisim}");
+
+//Console.ReadLine();
+
+namespace DERS__0_10_ //AD İSİM alanı
+{
+    internal class Program //SINIF ALANI
+    {
+        string isim = "Berkay";
+        if (isim == "berkay")
+        {
+	        string soyisim = "Ercan";
+            Console.WriteLine($"{isim}");
+        }
+            Console.WriteLine($"{soyisim}");
+    }
+}
+
+//namespace internetsitesi
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string ifade = "internet sitesi";
+
+//        }
+
+//        static string Yeniden(string mesajımız)
+//        {
+//            char[] isim = mesajımız.ToCharArray();
+//        }
+//    }
+
+//}
+
+//namespace internetsitesi.tasarım
+//{
+
+//}
+
+
