84e6742da0feb19ea4991c441cab14cb760db1cd
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-03-31 03:07:09 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,128 @@
-﻿using System;
+﻿//string isim = null;
+
+//if(isim=="Ali")
+//{
+//    string soyisim = "Abc";
+//    Console.WriteLine($"{isim}");
+//    Console.WriteLine($"{soyisim}");
+//}
+
+
+//Console.ReadLine();
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//string isim = "berkay";
-//string soyisim ="ercan";
 
-//if (isim == "berkay")
-//{
+//KOD BLOKLARI
 
-//    Console.WriteLine($"{isim}");
-//    Console.WriteLine($"{soyisim}");
-//}
-//Console.WriteLine($"{soyisim}");
-
-//Console.ReadLine();
-
-namespace DERS__0_10_ //AD İSİM alanı
+namespace DERS__0_10__ // AD ALANIMIZ
 {
-    internal class Program //SINIF ALANI
+    class Program // SINIF ALANIMIZ
     {
-        string isim = "Berkay";
-        if (isim == "berkay")
+        static void Main(string[] args) // YÖNTEM ALANI
         {
-	        string soyisim = "Ercan";
-            Console.WriteLine($"{isim}");
+            string name = "hasa";
+            if (name == "hasan") Console.WriteLine("hasani bulduk");
+            else if (name == "ali") Console.WriteLine("aliyi bulduk");
+            else Console.WriteLine("baskasini bulduk");
+            Console.ReadLine();
         }
-            Console.WriteLine($"{soyisim}");
     }
 }
 
-//namespace internetsitesi
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            string ifade = "internet sitesi";
+//aynı sınıftaki bir yöntemi çağırma
 
+//namespace internetsitesi  //ad alani
+//{
+//    class Program// sinifimiz
+//    {
+//        static void Main(string[] args) // yontemimiz
+//        {
+//            string isim = "Berkay";
+//            string cagrilan = cagirma(isim);
+//            Console.WriteLine($"{cagrilan}");
+//            Console.ReadLine();
 //        }
 
-//        static string Yeniden(string mesajımız)
+//        static string cagirma(string message)
 //        {
-//            char[] isim = mesajımız.ToCharArray();
+//            char[] dizi = message.ToCharArray();
+//            Array.Reverse(dizi);
+//            return new string(dizi);
+//        }
+
+//    }
+//} // AYNI SINIF İÇERİSİNDEKİ FARKLI YÖNTEMLERE SADECE YÖNTEMİN İSMİNİ KULLANARAK ÇAĞIRABİLİRİZ
+
+//ToCharArray bir veriyi karakterlerine ayirip char tipinde tanimlanmis bir diziye aktarir
+//Array.Reverse(dizi); dizi elemanlarimi tersine cevirdi
+//return ile yeniden olkusturulan dizimiz yenilenmis sekilde programa yeniden donderildi
+
+
+
+// FARKLI SINIFLAR ARASI YÖNTEM ÇAĞIRMA
+
+//using System;
+
+//namespace MyNewApp
+//{
+//    class Program// sinifimiz
+//    {
+//        static void Main(string[] args) // yontemimiz
+//        {
+//            string isim = "Berkay";
+//            string cagrilan = program2.cagirma(isim);
+//            Console.WriteLine($"{cagrilan}");
+//            Console.ReadLine();
 //        }
 //    }
 
+//    class program2
+//    {
+//        public static string cagirma(string message)
+//        {
+//            char[] dizi = message.ToCharArray();
+//            Array.Reverse(dizi);
+//            return new string(dizi);
+//        }
+//    }
 //}
 
-//namespace internetsitesi.tasarım
-//{
 
+
+//FARKLI AD ALANLARI ARASI YÖNTEM ÇAĞIRMA
+
+
+//namespace internetsitesi  //ad alani
+//{
+//    class Program// sinifimiz
+//    {
+//        static void Main(string[] args) // yontemimiz
+//        {
+//            string isim = "Berkay";
+//            string cagrilan = internetsitesi.tasarim.program2.cagirma(isim);
+//            Console.WriteLine($"{cagrilan}");
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//namespace internetsitesi.tasarim
+//{
+//    class program2
+//    {
+//       public static string cagirma(string message)
+//        {
+//            char[] dizi = message.ToCharArray();
+//            Array.Reverse(dizi);
+//            return new string(dizi);
+//        }
+//    }
 //}
 
 
